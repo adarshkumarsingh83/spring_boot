9bb07e6adfffdc12bf83445267cfd132c43a010c
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2016-05-12 23:19:34 +0530

    SpringBootJpaExample  SpringBootWebMvcJspExample

---
+++ SpringBootJpaExample/src/main/java/com/espark/adarsh/repository/EmployeeRepository.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Espark And ©Adarsh Development Services @copyright All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Espark nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.espark.adarsh.repository;
+
+import com.espark.adarsh.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+    public  List<Employee> findByLastName(String employeeLastName);
+
+    @Query(value="select employeeId,employeeFirstName,employeeLastName,employeeEmail,employeeSalary from Employee e where e.employeeFirstName=?1")
+    public List<Employee> findByEmployeeFirstName(String employeeFirstName);
+
+    @Query(value="select employeeId,employeeFirstName,employeeLastName,employeeEmail,employeeSalary from Employee e where e.employeeFirstName=?1 and e.employeeSalary=?2")
+    public List<Employee> findByEmployeeNameAndSalary(String employeeFirstName,long employeeSalary);
+
+}
