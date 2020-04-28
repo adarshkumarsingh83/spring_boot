dbea4cc42f5e82482850f4e03d797ab3e2e6def6
Kumar <adarshkumarsingh83@gmail.com>
2020-04-28 09:31:20 -0500

    hikari jpa connetion pool sample

---
+++ SPRINGBOOT-JPA-SAMPLES/springboot-hikari-connection-pool/src/main/java/com/espark/adarsh/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.espark.adarsh.service;
+
+import com.espark.adarsh.entity.Employee;
+import com.espark.adarsh.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
