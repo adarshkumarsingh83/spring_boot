a7459f008c34b7e8c496cc01b355e57cbfca6e43
adarsh <us-guest@USMB113823.local>
2020-06-19 13:01:37 -0500

    adding working springboot maven dependency profile example

---
+++ springboot-maven-profiling/src/main/java/com/espark/adarsh/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.espark.adarsh.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
