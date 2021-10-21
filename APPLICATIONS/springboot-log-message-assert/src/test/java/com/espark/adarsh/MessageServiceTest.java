c0b7b1c4b7e58a8c883b10b78b7410e0680c13dc
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-10-20 21:56:47 -0500

    springboot-test-mockito-test-log-message-assert

--- APPLICATIONS/springboot-log-message-assert/src/test/java/com/espark/adarsh/MessageServiceTest.java
+++ APPLICATIONS/springboot-log-message-assert/src/test/java/com/espark/adarsh/MessageServiceTest.java
@@ -4,18 +4,33 @@ import com.espark.adarsh.service.MessageService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import static org.mockito.Mockito.*;
+
 @ExtendWith({OutputCaptureExtension.class, MockitoExtension.class})
 public class MessageServiceTest {
+
+
+    private static final String input = "adarsh";
+
+    @Test
+    public void testGetMessage() {
+        MessageService messageService = Mockito.mock(MessageService.class);
+        when(messageService.getMessage(input)).thenReturn(MessageService.template + input);
+        String response = messageService.getMessage(input);
+        Assertions.assertThat(response).isEqualTo(MessageService.template + input);
+    }
 
     @Test
     public void testGetMessage(CapturedOutput output) {
         MessageService messageService = new MessageService();
-        messageService.getMessage("adarsh");
-        Assertions.assertThat(output).contains(MessageService.template + "adarsh");
+        when(messageService.getMessage(input)).thenReturn(MessageService.template + input);
+        String response = messageService.getMessage(input);
+        Assertions.assertThat(response).isEqualTo(MessageService.template + input);
+        Assertions.assertThat(output).contains(MessageService.template + input);
     }
 }
