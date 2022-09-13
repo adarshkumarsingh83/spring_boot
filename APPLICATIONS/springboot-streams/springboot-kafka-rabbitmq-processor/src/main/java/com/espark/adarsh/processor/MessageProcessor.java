941a2aecc316860bcde31e144274ef200d1009ed
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2022-09-13 17:18:35 -0500

    testing after rabbit and kafka locally working

--- APPLICATIONS/springboot-streams/springboot-kafka-rabbitmq-processor/src/main/java/com/espark/adarsh/processor/MessageProcessor.java
+++ APPLICATIONS/springboot-streams/springboot-kafka-rabbitmq-processor/src/main/java/com/espark/adarsh/processor/MessageProcessor.java
@@ -1,16 +1,21 @@
 package com.espark.adarsh.processor;
 
 import com.espark.adarsh.bean.MessageBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class MessageProcessor {
 
     @Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
     public Object transform(MessageBean messageBean) {
-        messageBean.setMessage(messageBean.getMessage().toUpperCase() + "_PROCESSED");
+        log.info("Message Bean Before Processing {}", messageBean);
+        String temp = messageBean.getMessage().toUpperCase() + "_PROCESSED";
+        messageBean.setMessage(temp);
+        log.info("Message Bean After Processing {}", messageBean);
         return messageBean;
     }
 }
