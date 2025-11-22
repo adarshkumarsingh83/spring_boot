b1f4b0ca9c54ad29f74eefc78a5024d9c61b162f
adarsh kumar <adarshkumar@adarshs-MacBook-Pro-16-new.local>
2025-11-22 17:27:35 -0600

    testing

--- APPLICATIONS/spring-boot-custom-bean-registration/src/main/java/com/espark/adarsh/service/MyService.java
+++ APPLICATIONS/spring-boot-custom-bean-registration/src/main/java/com/espark/adarsh/service/MyService.java
@@ -2,17 +2,16 @@ package com.espark.adarsh.service;
 
 import com.espark.adarsh.service.util.ServiceUtil;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Getter
+@RequiredArgsConstructor
 public class MyService {
 
     private final ServiceUtil serviceUtil;
 
-    public MyService(ServiceUtil serviceUtil) {
-        this.serviceUtil = serviceUtil;
-    }
 
     public String getWish(String name) {
         String response = serviceUtil.getWishFunction().apply(name);
